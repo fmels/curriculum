9970fbc26c726ff71986df1d81d7de894c4b4115
fmels <niefloyd@hotmail.com>
2018-03-26 14:48:25 +0200

    goed werkende versie

---
+++ My CV/mycv.js
@@ -0,0 +1,21 @@
+
+document.querySelectorAll(".nav-item").forEach(function(drukKnop) {
+    drukKnop.addEventListener("click", handleClickEvent, false);
+  });
+
+  function handleClickEvent(event) {
+
+    // const element = event.target
+
+    // if (!element.classList.hasClass("active")) {
+    //     element.classList.add("active")
+    // }
+
+    // let el = evt.target;
+   
+    // if (el.getAttribute("aria-selected") == "true") {
+    //     el.setAttribute("aria-selected", "false");
+    // } else {
+    //     el.setAttribute("aria-selected", "true");
+    // }
+  }
