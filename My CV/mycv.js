e59d4d54fecf507792087720543f23cd8a559db6
fmels <niefloyd@hotmail.com>
2018-03-28 09:19:19 +0200

    begin woensdag

--- My CV/mycv.js
+++ My CV/mycv.js
@@ -13,3 +13,31 @@ fetch(`${baseURL}/users/fmels/repos`)
         }
     })
 
+// const namesElement = document.getElementById('names');
+// const nameInput = document.getElementById('name');
+// const namesList = [
+//     [Dutch: native],
+//     [English: fluent],
+//     [German: fluent],
+//     [Russian: adequate],
+//     [French: adequate],
+//     [Polish: elementary],
+//     [Spanish: elementary]
+// ];
+
+// document.getElementById('enter').addEventListener('click', function() {
+//     const newName = nameInput.value.trim();
+//     // Only add when we have a value.
+//     if (newName) {
+//       namesList.push(newName);
+//       for (var i = 0, n; n = namesList[i]; i++) {
+//           var liElement = document.createElement('li');
+//           liElement.innerText = n;
+//       }
+//       namesElement.appendChild(liElement);
+//     }
+//     // Let's blank out the input ready for the next
+//     nameInput.value = '';
+//     // ...and focus it!
+//     nameInput.focus();
+// });
